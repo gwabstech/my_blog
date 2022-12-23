229f44ab812438000d9ffe295a2b2083bdbb8306
Abubakar <gwabstech@gmail.com>
2022-12-23 05:34:18 +0100

    click event

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,12 @@
 const Home = (probs) => {
+    function handleClick(e){
+       alert("You Click!",e);
+    }
     return (  
         <div className="home">
             <h2>This is the home page {probs.number} </h2>
+            <button onClick={(e)=> handleClick(e)}
+            style={{ backgroundColor: "blue", borderRadius: "10px", margin: "10px" }}>Click me</button>
         </div>
     );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,16 @@
 const NavBar = (props) => {
-    return ( 
+    return (
         <nav className="navbar">
             <h1> Gwabstech Solutions</h1>
             <div className="links">
-                <a href={props.homeLink}>Home </a>
-                <a href={props.contactUsLink}> Contact Us</a>
-                <a href={props.aboutLink}> About Us </a>
-                <a href={props.projectsLink}> Projects </a>
+                <a href={props.homeLink} style={{ backgroundColor: "white", borderRadius: "10px", margin: "10px" }}>Home </a>
+                <a href={props.contactUsLink} style={{ backgroundColor: "white", borderRadius: "10px", margin: "10px" }}> Contact Us</a>
+                <a href={props.aboutLink} style={{ backgroundColor: "white", borderRadius: "10px", margin: "10px" }}> About Us </a>
+                <a href={props.projectsLink} style={{ backgroundColor: "white", borderRadius: "10px", margin: "10px" }}> Projects </a>
             </div>
 
         </nav>
-     );
+    );
 }
- 
+
 export default NavBar;
